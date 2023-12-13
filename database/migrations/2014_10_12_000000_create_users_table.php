d41c87a73303de40c577ff77e1a0f565f52f1b4e
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2023-12-13 11:05:51 +0700

    templating

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->enum('role', ['admin', 'mentor', 'user'])->default('user');
             $table->date('dob')->nullable();
             $table->string('country')->default('Indonesia');
+            $table->string('avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Skydash Admin</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{ asset('backend/vendors/feather/feather.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/vendors/ti-icons/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/vendors/css/vendor.bundle.base.css') }}">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="{{ asset('backend/css/vertical-layout-light/style.css') }}">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="{{ asset('backend/images/favicon.png') }}" />
+</head>
+
+<body>
+    <div class="container-scroller">
+        @include('components.navbar')
+
+        <div class="container-fluid page-body-wrapper">
+
+            @include('components.sidebar')
+
+            <!-- partial -->
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    @yield('content')
+                </div>
+                <!-- content-wrapper ends -->
+                @include('components.footer')
+            </div>
+            <!-- main-panel ends -->
+        </div>
+        <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="{{ asset('backend/vendors/js/vendor.bundle.base.js') }}"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="{{ asset('backend/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('backend/js/hoverable-collapse.js') }}"></script>
+    <script src="{{ asset('backend/js/template.js') }}"></script>
+    <script src="{{ asset('backend/js/settings.js') }}"></script>
+    <script src="{{ asset('backend/js/todolist.js') }}"></script>
+    <!-- endinject -->
+</body>
+
+</html>
