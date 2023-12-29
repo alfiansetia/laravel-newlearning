90457b2e831c27b013bad34196b3c77da67a9995
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2023-12-29 07:59:33 +0700

    disable https

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    {{-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> --}}
 
     <title>{{ $title }} || {{ $company->name }}</title>
     <!-- plugins:css -->
