4ce3aecbe1d9d0e76c240d2fd4221a9aa364359d
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2023-12-30 19:19:59 +0700

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -42,7 +42,7 @@ class LoginController extends Controller
     protected function authenticated(Request $request, $user)
     {
         if ($user->role === 'user') {
-            return redirect()->route('index');
+            return redirect()->route('index.category');
         }
     }
 }
