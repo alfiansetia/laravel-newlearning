27b6efd2505d5d6b652ac926b743935b6ad0f72c
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2024-01-12 16:30:57 +0700

    Update navbar.blade.php

--- resources/views/components/frontend/navbar.blade.php
+++ resources/views/components/frontend/navbar.blade.php
@@ -28,23 +28,20 @@
         </ul>
         {{-- <div class="header__cart__price">item: <span>$150.00</span></div> --}}
     </div>
-    <div class="humberger__menu__widget">
+    {{-- <div class="humberger__menu__widget">
         <div class="header__top__right__language">
-            {{-- <img src="{{ asset('frontend/img/language.png') }}" alt="">
+            <img src="{{ asset('frontend/img/language.png') }}" alt="">
             <div>English</div>
             <span class="arrow_carrot-down"></span>
             <ul>
                 <li><a href="#">Spanis</a></li>
                 <li><a href="#">English</a></li>
-            </ul> --}}
+            </ul>
         </div>
-        @auth
-        @else
-            <div class="header__top__right__auth">
-                <a href="{{ route('login') }}"><i class="fa fa-user"></i> Login</a>
-            @endauth
+        <div class="header__top__right__auth">
+            <a href="{{ route('login') }}"><i class="fa fa-user"></i> Login</a>
         </div>
-    </div>
+    </div> --}}
     <nav class="humberger__menu__nav mobile-menu">
         {{-- <ul>
             <li class="active"><a href="{{ route('index.category') }}">Home</a></li>
@@ -62,17 +59,17 @@
         </ul> --}}
     </nav>
     <div id="mobile-menu-wrap"></div>
-    <div class="header__top__right__social">
-        {{-- <a href="#"><i class="fa fa-facebook"></i></a>
+    {{-- <div class="header__top__right__social">
+        <a href="#"><i class="fa fa-facebook"></i></a>
         <a href="#"><i class="fa fa-twitter"></i></a>
         <a href="#"><i class="fa fa-linkedin"></i></a>
-        <a href="#"><i class="fa fa-pinterest-p"></i></a> --}}
-    </div>
-    <div class="humberger__menu__contact">
-        {{-- <ul>
+        <a href="#"><i class="fa fa-pinterest-p"></i></a>
+    </div> --}}
+    {{-- <div class="humberger__menu__contact">
+        <ul>
             <li><i class="fa fa-envelope"></i> hello@colorlib.com</li>
             <li>Free Shipping for all Order of $99</li>
-        </ul> --}}
-    </div>
+        </ul>
+    </div> --}}
 </div>
 <!-- Humberger End -->
